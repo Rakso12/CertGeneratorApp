cf08a06e661d9e241926e98e2f54773968ace6fe
Mateusz Aliyev <82393815+mateuszaliyev@users.noreply.github.com>
2021-12-24 14:37:06 +0100

    fix: generate certificate on button click

--- script.js
+++ script.js
@@ -1,59 +1,64 @@
-window.jsPDF = window.jspdf.jsPDF
+window.jsPDF = window.jspdf.jsPDF;
 
 function generateCertificate(img, text) {
-    const canvas = document.getElementById('canvas');
-    const ctx = canvas.getContext('2d');
-  
-    canvas.width = img.width;
-    canvas.height = img.height;
-  
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.drawImage(img, 0, 0);
-  
-    ctx.fillStyle = '#21598c';
-    ctx.font = "italic 160px Lato";
-    ctx.textAlign = 'center';
+  const canvas = document.getElementById("canvas");
+  const ctx = canvas.getContext("2d");
 
-    ctx.fillText(text, canvas.width / 2, canvas.height/2 + 200, canvas.width);
+  canvas.width = img.width;
+  canvas.height = img.height;
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.drawImage(img, 0, 0);
+
+  ctx.fillStyle = "#21598c";
+  ctx.font = "italic 160px Lato";
+  ctx.textAlign = "center";
+
+  ctx.fillText(text, canvas.width / 2, canvas.height / 2 + 200, canvas.width);
 }
-  
-window.addEventListener('DOMContentLoaded', () => {
-    const textInput = document.getElementById('text');
-    const imageInput = document.getElementById('image-input');
-    const generateBtn = document.getElementById('generate-btn');
 
-    textInput.value = 'Adam Nowak';
+window.addEventListener("DOMContentLoaded", async () => {
+  const imageInput = document.getElementById("image-input");
+  const generateBtn = document.getElementById("generate-btn");
+  const inputXlsx = document.getElementById("excel-input");
 
-    generateBtn.addEventListener('click', () => {
-        const reader = new FileReader();
-        var pdf = new jsPDF({
-            orientation: 'l',
-        });
+  let person = [];
 
-                
+  let index = -1;
 
-        reader.onload = () => {
-            const img = new Image();
-            img.src = reader.result;
-            img.onload = () => {
-                let inputXlsx = document.getElementById('excel-input');
+  inputXlsx.addEventListener("change", async () => {
+    const json = await readXlsxFile(inputXlsx.files[0]);
+    person = json.map(([firstName, lastName]) => `${firstName} ${lastName}`);
+  });
 
-                async function myFunc() {
-                    json = await readXlsxFile(inputXlsx.files[0])
-                    person = json.map(([firstName, lastName]) => `${firstName} ${lastName}`);
-                    
-                    person.forEach(async (element) => {
-                        console.log(element);
-                        generateCertificate(img, element);
-                        pdf.addImage(canvas, 'JPEG', 0, 0, pdf.internal.pageSize.getWidth(), pdf.internal.pageSize.getHeight(), 'NONE');        
-                        await pdf.save(element +'.pdf');
-                        console.log(pdf.save);
-                    });
-                };
+  generateBtn.addEventListener("click", () => {
+    if (++index >= person.length) {
+      return;
+    }
 
-                myFunc(); 
-            };
-        };
-        reader.readAsDataURL(imageInput.files[0]);
+    const reader = new FileReader();
+    var pdf = new jsPDF({
+      orientation: "l",
     });
+
+    reader.onload = () => {
+      const img = new Image();
+      img.src = reader.result;
+      img.onload = async () => {
+        generateCertificate(img, person[index]);
+        pdf.addImage(
+          canvas,
+          "JPEG",
+          0,
+          0,
+          pdf.internal.pageSize.getWidth(),
+          pdf.internal.pageSize.getHeight(),
+          "NONE"
+        );
+        await pdf.save(person[index] + ".pdf");
+      };
+    };
+
+    reader.readAsDataURL(imageInput.files[0]);
+  });
 });
